fa777770d7b088b29d75fabdf848986f9862c39f
Alexander Kleimenov <nequamy@gmail.com>
2024-10-07 17:09:33 +0300

    update git cache

---
+++ src/lib/stream.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+
+class Stream(object):
+    def __init__(self, mode: str = None):
+        self.cap = None
+        self.ids = None
+        self.url = "http://192.168.2.113:81/stream"
+        self.mode = mode
+
+        self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
+        self.parameters = cv2.aruco.DetectorParameters()
+
+    def stream(self):
+        self.cap = cv2.VideoCapture(self.url)
+
+        if not self.cap.isOpened():
+            print("Ошибка: Не удалось открыть поток видео.")
+            exit()
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                print("Ошибка: Не удалось получить кадр.")
+                break
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            corners, self.ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, self.aruco_dict,
+                                                                           parameters=self.parameters)
+        
+            
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def get_id(self):
+        return self.ids
+
---
+++ src/main.py
@@ -0,0 +1,81 @@
+from src.app import Api
+from lib.drone import ARA_mini
+from lib.stream import Stream
+
+from threading import Thread
+import time
+
+"""
+Список методов API:
+
+api.set_arm_state(state) - принимает на вход True или False для ARM и DISARM соответственно.
+api.set_nav_state(state) - принимает на вход 0, 1 и 2 для ANGLE, ALTHOLD и POSHOLD соответственно.
+api.takeoff(altitude) - метод для подъема дрона на высоту, отправленную в аргументах функции.
+api.land(auto_disarm) - метод для опускания дрона. На вход ожидает True или False для автоматического
+                        отключения двигателей.
+api.go_to_xy(x, y, auto_land) - метод отправки дрона в точку (Х, Y). В случае если задан auto_land
+                                дрон после перемещения в эту точку снизит высоту и выключит движки.
+api.set_velocity_x(x_vel) - метод для задания скорости перемещения в оси Х.
+api.set_velocity_y(y_vel) - метод для задания скорости перемещения в оси Y.
+api.set_throttle(throttle) - метод для задания уровня газа/высоты коптера. Значение 2000 - 2.5м, 1000 - 0м.
+
+Список методов Stream:
+stream.get_id() - возвращает id всех распознанных маркеров.
+
+Поля для вывода:
+api.odom - словарь со значениями одометрии(['position'], ['velocity'], ['yaw'].
+api.attitude - структура из папки data с ориентацией дрона в углах эйлера и в кватернионе.
+api.imu - структура из папки data со значениями IMU-датчика.
+api.rc_out - структура из папки data со значениями RC-каналлов, подаваемых на полетный контроллер.
+
+P.s. строчки над "Начало кода" и под "Конец кода" оставляем не тронутыми.
+"""
+
+
+def main():
+    drone = ARA_mini()
+    api = Api("192.168.2.113", 5760, drone)
+    stream = Stream()
+
+    stream_thread = Thread(target=stream.stream, args=(), daemon=True)
+    update_thread = Thread(target=api.update_loop, args=(), daemon=True)
+    stream_thread.start()
+    update_thread.start()
+
+    time.sleep(1)
+
+    ######## Начало кода ########
+
+    # Пример
+
+    api.set_arm_state(1)
+    time.sleep(2)
+    api.set_nav_state(2)
+    time.sleep(2)
+    api.takeoff(1.5)
+    time.sleep(15)
+    api.land()
+
+    print(api.go_to_xy(1, 1))
+
+    # while not (stream.get_id() == 1):
+    #     api.set_velocity_x(1)
+    # else:
+    #     api.set_velocity_x(0)
+    #
+    # while not (stream.get_id() == 2):
+    #     api.set_velocity_y(1)
+    # else:
+    #     api.set_velocity_y(0)
+
+    ######## Конец кода ########
+
+    stream_thread.join()
+    update_thread.join()
+
+    api.reset_state()
+    api.load_data()
+
+
+if __name__ == "__main__":
+    main()
