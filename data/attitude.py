1f826a8316beabf865d18c97524f77067d892358
Alexander Kleimenov <nequamy@gmail.com>
2024-10-09 16:02:16 +0300

    fix dev branch after merge

--- data/attitude.py
+++
@@ -1,7 +0,0 @@
-from data.quaternion import Quaternion
-from data.vector import Vector
-
-class Attitude:
-    def __init__(self):
-        self.orientation = Quaternion()
-        self.body_rate = Vector()
