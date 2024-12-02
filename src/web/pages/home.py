b322abb5288b4c7c8f667d508266020c516f17e4
Alexander Kleimenov <nequamy@gmail.com>
2024-12-02 21:28:44 +0300

    pre-release web-app preparation

--- src/web/pages/home.py
+++ src/web/pages/home.py
@@ -5,7 +5,16 @@ import dash_mantine_components as dmc
 dash.register_page(__name__, path='/')
 
 layout = dmc.Center(
-    html.Div("Пока что тут ничего нет, меня добавят в будущих версиях", style={'textAlign': 'center'})
+    html.Div(
+        "Пока что тут ничего нет, меня добавят в будущих версиях",
+        style={
+            'display': 'flex',
+            'justifyContent': 'center',
+            'alignItems': 'center',
+            'height': '100vh',
+            'textAlign': 'center'
+        }
+    )
 )
 
 # TODO: организовать домашнюю веб страницу с описанием каждого дрона и с навигацией по веб интерфейсу
