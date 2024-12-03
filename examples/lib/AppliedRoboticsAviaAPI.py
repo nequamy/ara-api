3840ca73482eed8a1585099dd946022d8f23bf58
Alexander Kleimenov <nequamy@gmail.com>
2024-12-03 22:49:50 +0300

    [+] simple Application Programming Interface for users

--- examples/lib/AppliedRoboticsAviaAPI.py
+++ examples/lib/AppliedRoboticsAviaAPI.py
@@ -1,47 +1,85 @@
 import asyncio
-from navigation.nav_service import NavigationManagerGRPC
-from driver.msp_service import MSPServiceClient
+import grpc
+import protos.api_pb2 as api_pb2
+from protos.api_pb2_grpc import DriverManagerStub, NavigationManagerStub
+
+# TODO: написать рабочую библиотеку для управления дроном с помощью RPC
 
 class AppliedRoboticsAviaAPI:
     """
     Provides an interface to call RPC services sequentially.
     """
 
-    def __init__(self):
+    def __init__(self, nav_address='localhost:50052', driver_address='localhost:50051'):
         """
         Initializes the AppliedRoboticsAviaAPI.
         """
-        self.nav_client = NavigationManagerGRPC()
-        self.msp_client = MSPServiceClient()
+        self.nav_channel = grpc.insecure_channel(nav_address)
+        self.driver_channel = grpc.insecure_channel(driver_address)
 
-    async def takeoff(self, altitude):
-        """
-        Calls the takeoff service.
-        """
-        response = await self.nav_client.Takeoff(altitude)
-        print(f"Takeoff response: {response}")
+        self.nav_stub = NavigationManagerStub(self.nav_channel)
+        self.driver_stub = DriverManagerStub(self.driver_channel)
 
-    async def move_by_point(self, x, y, z):
+    def takeoff(self, altitude):
         """
-        Calls the move_by_point service.
+        Calls the takeoff service from NavigationManagerGRPC
         """
-        response = await self.nav_client.MoveByPoint(x, y, z)
-        print(f"Move by point response: {response}")
+        request = api_pb2.AltitudeSetData(altitude=altitude)
+        response = self.nav_stub.TakeOFF(request)
+        return response.status
 
-    async def land(self):
+    def land(self):
         """
-        Calls the land service.
+        Calls the land service from NavigationManagerGRPC
         """
-        response = await self.nav_client.Land()
-        print(f"Land response: {response}")
+        request = api_pb2.AltitudeSetData(altitude=0)
+        response = self.nav_stub.Land(request)
+        return response.status
 
-    def __getattr__(self, name):
+    def move_by_point(self, x, y, z):
         """
-        Dynamically handles method calls.
+        Calls the move service from NavigationManagerGRPC
         """
-        async def method(*args, **kwargs):
-            if name in self.__class__.__dict__:
-                await self.__class__.__dict__[name](self, *args, **kwargs)
-            else:
-                raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
-        return method
+        request = api_pb2.MoveRequest(x=x, y=y, z=z)
+        response = self.nav_stub.Move(request)
+        return response.status
+
+
+    def set_velocity(self, vx, vy, vz):
+        """
+        Calls the set_speed service from NavigationManagerGRPC
+        """
+        request = api_pb2.SetVelocityRequest(vx=vx, vy=vy, vz=vz)
+        response = self.nav_stub.SetVelocity(request)
+        return response.status
+
+    def get_imu_data(self):
+        response = self.driver_stub.GetImuDataRPC(api_pb2.GetRequest(req=''))
+        return response
+
+    def get_sonar_data(self):
+        response = self.driver_stub.GetSonarDataRPC(api_pb2.GetRequest(req=''))
+        return response
+
+    def get_attitude_data(self):
+        response = self.driver_stub.GetAttitudeDataRPC(api_pb2.GetRequest(req=''))
+
+        return {
+            'orientation': (response.orient.x, response.orient.y, response.orient.z),
+        }
+
+    def get_odometry_data(self):
+        response = self.driver_stub.GetOdometryDataRPC(api_pb2.GetRequest(req=''))
+
+        return {
+            'position': (response.pos.x, response.pos.y, response.pos.z),
+            'velocity': (response.vel.x, response.vel.y, response.vel.z)
+        }
+
+    def get_optical_flow_data(self):
+        response = self.driver_stub.GetOpticalFlowDataRPC(api_pb2.GetRequest(req=''))
+        return response
+
+    def get_flags_data(self):
+        response = self.driver_stub.GetFlagsDataRPC(api_pb2.GetRequest(req=''))
+        return response
